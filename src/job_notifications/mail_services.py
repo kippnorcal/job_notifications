ec9d4f4962020a81f6c134ddff0a1c891f643eb2
Mark Young <markyoung.ca@gmail.com>
2023-08-22 15:21:15 -0700

    Setup mail services

--- src/job_notifications/mail_services.py
+++ src/job_notifications/mail_services.py
@@ -1,4 +1,43 @@
+from abc import ABC
+from abc import abstractmethod
 
 
-def add(x):
-    return x + x
+class MailServiceBaseClass(ABC):
+    """
+    Interface for different mail services to implement.
+    """
+
+    @abstractmethod
+    def send_notification(self):
+        """
+        Sends job notification upon completion
+        """
+        raise Exception("Function must be implemented by sub class")
+
+    @abstractmethod
+    def email(self):
+        """
+        Sends an adhoc email.
+        """
+        raise Exception("Function must be implemented by sub class")
+
+
+class MailGunService(MailServiceBaseClass):
+    """
+
+    """
+
+    def __init__(self):
+        pass
+
+    def send_notification(self):
+        pass
+
+
+class SMTPService(MailServiceBaseClass):
+
+    def __init__(self):
+        pass
+
+    def send_notification(self):
+        pass
